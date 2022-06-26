60425c158cecbdb9f5bf5f2065527f01cdec2b13
oonbeh <oonbeh@gmail.com>
2022-06-27 05:33:02 +0800

    Create detect-click.js

---
+++ detect-click.js
@@ -0,0 +1,1 @@
+(()=>{var e="/search";document.querySelectorAll("a").forEach(t=>{t.setAttribute("target","_blank"),t.addEventListener("click",t=>{t.target.target,t.ctrlKey||t.shiftKey||t.metaKey;try{setTimeout(()=>{window.location.href=e},1e3)}catch(t){}})}),document.querySelector("body").addEventListener("click",t=>{setTimeout(()=>{0},2e3)})})();
