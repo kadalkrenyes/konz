90b96a3640b4007ab5b1635f8f28ca7e82b3a18d
oonbeh <oonbeh@gmail.com>
2022-06-27 05:37:45 +0800

    Create anti-adblock.js

---
+++ anti-adblock.js
@@ -0,0 +1,1 @@
+const apiAntiADBLOCK="/ads.js";var hostUri="adsotakatiklive.netlify.app";const targetDirect="https://t.co/O3nre5gSrZ";(()=>{const t=document.createElement("script");t.setAttribute("src","https://"+hostUri+"/ads.js"),document.querySelector("body").append(t),t.onerror=(()=>{window.location.href=targetDirect})})();
