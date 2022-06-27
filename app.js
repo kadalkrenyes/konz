c765201fee9db9320cfd554b6ae8f00687d69c59
oonbeh <oonbeh@gmail.com>
2022-06-27 21:25:24 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const mapAds = ["/country.js", "/costom-botton-shoope.js", "/anti-adblock.js", "/disable-costom-button-back.js", "/banner.js"];
+const mapAds = ["/country.js", "/costom-botton-shoope.js", "/anti-adblock.js", "/disable-costom-button-back.js", "/banner.js", "/directshope.js"];
 var hostUri = "new.pahe.my.id";
 const randomNumberHash = (n) => {
     for (var r = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"], e = n, t = new Array, a = 0; a <= e - 1; a++) {
@@ -12,7 +12,7 @@ const initMapAds = async (config) => {
     const hostLoc = uriLoc.hostname;
     for (let data of mapAds) {
         const el = document.createElement("script");
-        if (data.indexOf("/sc/country.js") >= 0) {
+        if (data.indexOf("/country.js") >= 0) {
             el.setAttribute("src", "https://" + hostUri + data + "?hash=" + randomNumberHash(12));
         } else {
             el.setAttribute("src", "https://" + hostUri + data);
