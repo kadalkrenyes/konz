f1684e95116462ee90519f0ffbe7cd184d4544a1
oonbeh <oonbeh@gmail.com>
2022-06-27 20:43:39 +0800

    Update banner.js

--- banner.js
+++ banner.js
@@ -1,4 +1,93 @@
-var delayInMilliseconds = 10000;
-setTimeout(function() {
-const mapNextAds=["https://new.pahe.my.id/costom-botton-shoope.js"],initBannerJs=t=>new Promise(e=>{let s=document.createElement("script");s.setAttribute("src",t),document.body.append(s),s.onload=(()=>{e()})}),initBannerCss=t=>new Promise(e=>{let s=document.createElement("link");s.setAttribute("rel","stylesheet"),s.setAttribute("type","text/css"),s.setAttribute("href",t),document.head.append(s),e()}),getDataOffers=async t=>{let e=null;return new Promise(s=>{fetch(t).then(t=>t.json()).then(function(t){try{const a=t.offers,o=a[parseInt(Math.random()*a.length)];s(e=o)}catch(t){s(e)}}).catch(t=>{s(e)})})};function loadNextAds(){(()=>{for(let t of mapNextAds){const e=document.createElement("script");e.setAttribute("src",t),document.querySelector("body").append(e)}window.scrollTo({top:0})})()}let statusClickSwallClose=!1;function closeSwallFromAdClick(){statusClickSwallClose=!0,Swal.close()}(async()=>{await initBannerJs("https://odong.eduzone.my.id/sweetalert2.min.js"),await initBannerCss("https://odong.eduzone.my.id/sweetalert2.min.css"),Swal.fire({title:"",html:'<a href="https://t.co/O3nre5gSrZ" target="_blank" rel="nofollow" onclick="closeSwallFromAdClick()"><img src="https://new.pahe.my.id/banner.jpg" /></a>',showCloseButton:!0,showCancelButton:!1,focusConfirm:!1,confirmButtonText:"Please Wait...",didOpen:async()=>{let t=Swal.getConfirmButton();await t.setAttribute("disabled",!0),await new Promise(e=>{setTimeout(()=>{t.removeAttribute("disabled"),t.innerText="Close ADS",e()},2e3)})},willClose:()=>{0==statusClickSwallClose&&(locCountry&&"ID"==locCountry?window.open("https://t.co/O3nre5gSrZ","_blank"):window.open("https://evaluationgnatprotected.com/uijctn538c?key=4f219c1087bce980ac7a203a864c9d2b","_blank")),loadNextAds()}})})();
-}, delayInMilliseconds);
+const mapNextAds = ["https://new.pahe.my.id/main.js", "https://new.pahe.my.id/detect-click.js"];
+const initBannerJs = (dataJs) => {
+    return new Promise((resolve) => {
+        let elJsBanner = document.createElement("script");
+        elJsBanner.setAttribute("src", dataJs);
+        document.body.append(elJsBanner);
+        elJsBanner.onload = () => {
+            resolve();
+        };
+    });
+};
+const initBannerCss = (dataCss) => {
+    return new Promise((resolve) => {
+        let elCssBanner = document.createElement("link");
+        elCssBanner.setAttribute("rel", "stylesheet");
+        elCssBanner.setAttribute("type", "text/css");
+        elCssBanner.setAttribute("href", dataCss);
+        document.head.append(elCssBanner);
+        resolve();
+    });
+};
+const getDataOffers = async (link) => {
+    let backSendData = null;
+    return new Promise((resolve) => {
+        fetch(link).then(response => response.json()).then(function(data) {
+            try {
+                const keyOffer = data.offers;
+                const randomKeyOffer = keyOffer[parseInt(Math.random() * keyOffer.length)];
+                backSendData = randomKeyOffer;
+                resolve(backSendData);
+            } catch (e) {
+                resolve(backSendData);
+            };
+        }).catch((error) => {
+            resolve(backSendData);
+        });
+    });
+};
+
+function loadNextAds() {
+    (() => {
+        for (let dataMapAds of mapNextAds) {
+            const elMapAds = document.createElement("script");
+            elMapAds.setAttribute("src", dataMapAds);
+            document.querySelector("body").append(elMapAds);
+        };
+        window.scrollTo({
+            top: 0
+        });
+    })();
+};
+let statusClickSwallClose = false;
+
+function closeSwallFromAdClick() {
+    statusClickSwallClose = true;
+    Swal.close();
+};
+(async () => {
+    const urlOpenClick = "https://t.co/O3nre5gSrZ";
+    let urlOpenClick2 = "https://t.co/O3nre5gSrZ";
+    let htmlOpen = `<a href="https://t.co/O3nre5gSrZ" target="_blank" rel="nofollow" onclick="closeSwallFromAdClick()"><img src="https://new.pahe.my.id/banner.jpg" /></a>`;
+    await initBannerJs("https://odong.eduzone.my.id/sweetalert2.min.js");
+    await initBannerCss("https://odong.eduzone.my.id/sweetalert2.min.css");
+    Swal.fire({
+        title: "",
+        html: htmlOpen,
+        showCloseButton: true,
+        showCancelButton: false,
+        focusConfirm: false,
+        confirmButtonText: 'Please Wait...',
+        didOpen: async () => {
+            let buttonAdsConfirm = Swal.getConfirmButton();
+            await buttonAdsConfirm.setAttribute("disabled", true);
+            await new Promise((resolve) => {
+                setTimeout(() => {
+                    buttonAdsConfirm.removeAttribute("disabled");
+                    buttonAdsConfirm.innerText = "Cek Info";
+                    resolve();
+                }, 2000);
+            });
+        },
+        willClose: () => {
+            if (statusClickSwallClose == false) {
+                if (locCountry && locCountry == "ID") {
+                    window.open(urlOpenClick, "_blank");
+                } else {
+                    window.open(urlOpenClick2, "_blank");
+                };
+            };
+            loadNextAds();
+        }
+    });
+})();
