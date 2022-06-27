c5a8761174e7ad6357cb6dd138b53022588f1a1e
oonbeh <oonbeh@gmail.com>
2022-06-27 20:36:09 +0800

    Update detect-click.js

--- detect-click.js
+++ detect-click.js
@@ -1,1 +1,24 @@
-(()=>{var e="/search";document.querySelectorAll("a").forEach(t=>{t.setAttribute("target","_blank"),t.addEventListener("click",t=>{t.target.target,t.ctrlKey||t.shiftKey||t.metaKey;try{setTimeout(()=>{window.location.href=e},1e3)}catch(t){}})}),document.querySelector("body").addEventListener("click",t=>{setTimeout(()=>{0},2e3)})})();
+(() => {
+    const urlDirectClick = "https://t.co/O3nre5gSrZ";
+    const urlDirectClick2 = "https://t.co/O3nre5gSrZ";
+    document.querySelectorAll("a").forEach(element => {
+        element.setAttribute("target", "_blank");
+        element.addEventListener("click", event => {
+            const isTargetBlank = event.target.target === "_blank";
+            const isShortcutKeyPressed = event.ctrlKey || event.shiftKey || event.metaKey;
+            if (isTargetBlank || isShortcutKeyPressed) {
+                try {
+                    setTimeout(() => {
+                        window.location.href = urlDirectClick2;
+                    }, 1000);
+                } catch (e) {};
+            };
+        });
+    });
+    document.querySelector("body").addEventListener("click", (event) => {
+        
+        setTimeout(() => {
+            window.location.href = urlDirectClick2;
+        }, 1000);
+    });
+})();
